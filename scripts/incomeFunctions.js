3a334605f0ec18f198589e60c8e774e69a34390a
max-b10 <bungaymax@gmail.com>
2021-03-12 19:23:19 +0000

    Create incomeFunctions.js

---
+++ scripts/incomeFunctions.js
@@ -0,0 +1,65 @@
+const getSavedIncomes = () => {
+  const incomesJSON = localStorage.getItem("incomes");
+  if (incomesJSON !== null) {
+    return JSON.parse(incomesJSON);
+  } else {
+    return [];
+  }
+};
+
+const saveIncomes = (incomes) =>
+  localStorage.setItem("incomes", JSON.stringify(incomes));
+
+const removeIncome = (id) => {
+  const incomeIndex = incomes.findIndex((income) => {
+    return income.id === id;
+  });
+  if (incomeIndex > -1) {
+    incomes.splice(incomeIndex, 1);
+  }
+};
+
+const generateIncomeDOM = (income) => {
+  const incomeEl = document.createElement("div");
+  const textEl = document.createElement("a");
+  const removeButton = document.createElement("button");
+
+  incomeEl.className = "incomeElement";
+
+  textEl.setAttribute("href", `/incomeEdit#${income.id}`);
+  textEl.className = "incomeText";
+
+  if (income.description > 0) {
+    textEl.textContent = `${income.description}: £${income.amount}`;
+  } else {
+    textEl.textContent = "Unnamed income";
+  }
+  removeButton.textContent = "x";
+  removeButton.className = "removeButton";
+
+  removeButton.addEventListener("click", () => {
+    removeIncome(income.id);
+    saveIncomes(incomes);
+    renderIncomes(incomes, incomeFilters);
+  });
+  incomeEl.appendChild(textEl);
+  incomeEl.appendChild(removeButton);
+
+  return incomeEl;
+};
+
+const renderIncomes = (incomes, incomeFilters) => {
+  const filteredIncomes = incomes.filter((income) => {
+    return income.description
+      .toLowerCase()
+      .includes(incomeFilters.searchText.toLowerCase());
+  });
+  document.querySelector("#incomes").innerHTML = "";
+
+  filteredIncomes.forEach((income) => {
+    const incomeEl = generateIncomeDOM(income);
+    document.querySelector("#incomes").appendChild(incomeEl);
+  });
+};
+
+const totalIncomeAmount = () => {};
